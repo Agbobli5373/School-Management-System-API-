6c4f0266e189268ec3a24cf88f760dc8120a6565
agbobli isaac <agbobliisaackwadzo@gmail.com>
2023-02-07 10:05:04 +0000

    Commmit

---
+++ server.js
@@ -0,0 +1,10 @@
+const http = require("http");
+const app = require('./app/app')
+
+const PORT = process.env.PORT || 2023
+
+//creating server
+const server = http.createServer(app)
+
+
+server.listen(PORT, () => console.log("Server is running"))
