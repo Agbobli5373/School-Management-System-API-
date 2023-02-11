7f66414be563613d3de109d29a7078c25b3d60a0
agbobli isaac <agbobliisaackwadzo@gmail.com>
2023-02-11 22:34:09 +0000

    adding createAcademicYearCtrl business logic

--- controller/Academics/academicTermCtrl.js
+++ controller/Academics/academicTermCtrl.js
@@ -34,8 +34,22 @@ exports.getAcademicTermsCtrl = AsynHandler( async (req,res)=>{
 //@Route PUT api/v1/academic-terms
 //@Access Private
 exports.updateAcademicTermCtrl = AsynHandler( async (req,res)=>{
-    res.status(200).json({
-        status : 'success'
+    const {name , description,duration} = req.body;
+    const academicTermExit = await AcademicTerm.findOne({name});
+    if(academicTermExit){
+        throw new Error("Academic Year Exit");
+    }
+
+    const createdAcademicTerm = await AcademicTerm.create({
+        name,
+        description,
+        duration,
+        createdBy : req.useAuth._id
+    })
+    res.status(201).json({
+        status : 'success',
+        message : "Academic Term Created Successfully",
+        data : createdAcademicTerm
      })
 })
 
