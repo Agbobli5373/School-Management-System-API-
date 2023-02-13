96a3366a1fc6d15d7113c7fc4ad965873f207118
agbobli isaac <agbobliisaackwadzo@gmail.com>
2023-02-13 07:44:08 +0000

    CRUD operation on yearGroup controller

--- controller/Academics/yearGroupCtrl.js
+++ controller/Academics/yearGroupCtrl.js
@@ -4,7 +4,7 @@ const Program = require("../../model/Academics/Program");
 const AsyncHandler = require("express-async-handler");
 
 //@Desc Create Year Group
-//@Route POST api/v1/year-group/
+//@Route POST api/v1/year-groups/
 //@Access Private
 exports.createYearGroupCtrl = AsyncHandler(async (req, res) => {
   const { name, academicYear } = req.body;
@@ -17,6 +17,11 @@ exports.createYearGroupCtrl = AsyncHandler(async (req, res) => {
     academicYear,
     createdBy: req.useAuth._id
   })
+  //push admin year group to admin model
+  const admin = Admin.findById(req,useAthu._id);
+  admin.yearGroups.push(createdYearGroup);
+  admin.save();
+
   res.status(201).json({
     status: "Success",
     message: "Year Group created Successful",
@@ -25,15 +30,64 @@ exports.createYearGroupCtrl = AsyncHandler(async (req, res) => {
 });
 
 //@Desc Get single Year Group
-//@Route POST api/v1/year-group/:id
+//@Route GET api/v1/year-groups/:id
 //@Access Private
-exports.createYearGroupCtrl = AsyncHandler(async (req, res) => {
+exports.getYearGroupCtrl = AsyncHandler(async (req, res) => {
     const yearGroup = await YearGroup.findById(req.params.id)
     res.status(201).json({
       status: "Success",
-      message: "Year Group created Successful",
+      message: "Year Group fetch Successful",
       data: yearGroup,
     });
   });
   
+//@Desc Get all Year Group
+//@Route GET api/v1/year-groups/
+//@Access Private
+exports.getYearGroupsCtrl = AsyncHandler(async (req, res) => {
+    const yearGroups = await YearGroup.find() ;
+    res.status(201).json({
+      status: "Success",
+      message: "Year Groups Fetch Successful",
+      data: yearGroups,
+    });
+  });
 
+//@Desc Update Year Group
+//@Route PUT api/v1/year-groups/:id
+//@Access Private
+exports.updateYearGroupCtrl = AsyncHandler(async (req, res) => {
+    const { name, academicYear } = req.body;
+    const YearGroupExit = await YearGroup.findOne({name}) ;
+    if(YearGroupExit){
+      throw new Error("Year group Exit Already");
+    } 
+    const updatedYearGroup = await YearGroup.findByIdAndUpdate(req.params.id ,{
+      name,
+      academicYear,
+      createdBy: req.useAuth._id
+    },{
+        new : true
+    })
+    res.status(201).json({
+      status: "Success",
+      message: "Year Group updated Successful",
+      data: updatedYearGroup,
+    });
+  });
+  
+//@Desc Delete Year Group
+//@Route DELETE api/v1/year-group/
+//@Access Private
+exports.deleteYearGroupCtrl = AsyncHandler(async (req, res) => {
+    const YearGroupFound = await YearGroup.findById(req.params.id) ;
+    if(!YearGroupFound){
+      throw new Error("Year group updating doesn't exit");
+    } 
+    const deletedYearGroup = await YearGroup.findByIdAndDelete(req.params.id)
+    res.status(201).json({
+      status: "Success",
+      message: "Year Group Deleted Successful"
+    });
+  });
+  
