969fd580df0565e5eb3e2e1780d5f615113ea333
agbobli isaac <agbobliisaackwadzo@gmail.com>
2023-02-14 09:20:04 +0000

    Adding teacher middleware to check login and isTeacher role

---
+++ middlewares/isTeacher.js
@@ -0,0 +1,18 @@
+
+const Teacher = require('../model/Staff/Teacher')
+
+const isTeacher = async (req, res, next) => {
+    const userId = req?.useAuth?._id;
+    //find a user
+    const userFound = await Teacher.findById(userId);
+    //check if is Teacher
+    if(userFound?.role ==='teacher'){
+        next()
+    }
+    else{
+        next(new Error('Access denied , Teacher only'))
+    }
+
+ };
+
+module.exports = isTeacher;
---
+++ middlewares/isTeacherLogin.js
@@ -0,0 +1,25 @@
+const verifyToken = require("../utils/verifyToken");
+const Teacher = require("../model/Staff/Teacher");
+
+const isTeacherLogin = async (req, res, next) => {
+  //get token from header
+  const headerObject = req.headers;
+  const token = headerObject?.authorization?.split(" ")[1];
+  //console.log(token);
+  //verify token
+  const verifiedToken = verifyToken(token);
+  if (verifiedToken) {
+    const user = await Teacher.findById(verifiedToken.id).select(
+      "email name role"
+    );
+    //save the user into req.body
+    req.useAuth = user;
+    next()
+   
+  } else {
+    const err = new Error("Token Expired");
+    next(err);
+  }
+};
+
+module.exports = isTeacherLogin;
